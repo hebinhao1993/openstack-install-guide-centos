05c19c37694c892064aa0639cdb5e05aeded16bb
hebinhao1993 <hebinhao1993@outlook.com>
2019-10-09 19:07:27 +0800

    modify install-etcd.sh

--- install-etcd.sh
+++ install-etcd.sh
@@ -1,2 +1,34 @@
 #!/usr/bin/env bash
 yum install -y etcd
+
+sed -i -e 's/^#ETCD_DATA_DIR/ETCD_DATA_DIR/' /etc/etcd/etcd.conf
+sed -i -e 's/^ETCD_DATA_DIR.*/ETCD_DATA_DIR="/var/lib/etcd/default.etcd"/' /etc/etcd/etcd.conf
+
+
+sed -i -e 's/^#ETCD_LISTEN_PEER_URLS/ETCD_LISTEN_PEER_URLS/' /etc/etcd/etcd.conf
+sed -i -e 's/^ETCD_LISTEN_PEER_URLS/ETCD_LISTEN_PEER_URLS="http://10.0.2.5:2380"/' /etc/etcd/etcd.conf
+
+
+sed -i -e 's/^#ETCD_LISTEN_CLIENT_URLS/ETCD_LISTEN_CLIENT_URLS/' /etc/etcd/etcd.conf
+sed -i -e 's/^ETCD_LISTEN_CLIENT_URLS/ETCD_LISTEN_CLIENT_URLS="http://10.0.2.5:2379"/' /etc/etcd/etcd.conf
+
+sed -i -e 's/^#ETCD_NAME/ETCD_NAME/' /etc/etcd/etcd.conf
+sed -i -e 's/^ETCD_NAME/ETCD_NAME="controller"/' /etc/etcd/etcd.conf
+
+sed -i -e 's/^#ETCD_INITIAL_ADVERTISE_PEER_URLS/ETCD_INITIAL_ADVERTISE_PEER_URLS/' /etc/etcd/etcd.conf
+sed -i -e 's/^ETCD_INITIAL_ADVERTISE_PEER_URLS/ETCD_INITIAL_ADVERTISE_PEER_URLS="http://10.0.2.5:2380"/' /etc/etcd/etcd.conf
+
+sed -i -e 's/^#ETCD_ADVERTISE_CLIENT_URLS/ETCD_ADVERTISE_CLIENT_URLS/' /etc/etcd/etcd.conf
+sed -i -e 's/^ETCD_ADVERTISE_CLIENT_URLS/ETCD_ADVERTISE_CLIENT_URLS="http://10.0.2.5:2379"/' /etc/etcd/etcd.conf
+
+sed -i -e 's/^#ETCD_INITIAL_CLUSTER/ETCD_INITIAL_CLUSTER/' /etc/etcd/etcd.conf
+sed -i -e 's/^ETCD_INITIAL_CLUSTER/ETCD_INITIAL_CLUSTER="controller=http://10.0.2.5:2380"/' /etc/etcd/etcd.conf
+
+sed -i -e 's/^#ETCD_INITIAL_CLUSTER_TOKEN/ETCD_INITIAL_CLUSTER_TOKEN/' /etc/etcd/etcd.conf
+sed -i -e 's/^ETCD_INITIAL_CLUSTER_TOKEN/ETCD_INITIAL_CLUSTER_TOKEN="etcd-cluster-01"/' /etc/etcd/etcd.conf
+
+sed -i -e 's/^#ETCD_INITIAL_CLUSTER_STATE/ETCD_INITIAL_CLUSTER_STATE/' /etc/etcd/etcd.conf
+sed -i -e 's/^ETCD_INITIAL_CLUSTER_STATE/ETCD_INITIAL_CLUSTER_STATE="new"/' /etc/etcd/etcd.conf
+
+systemctl enable etcd
+systemctl start etcd
