5f58def0721ce02756b2701e709515c9b99219d4
hebinhao1993 <hebinhao1993@outlook.com>
2019-10-10 19:21:16 +0800

    modify install-database.sh and ntp-compute.sh

--- install-database.sh
+++ install-database.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 yum install -y mariadb mariadb-server python2-PyMySQL
 
+if [ -e /etc/my.cnf.d/openstack.cnf ]; then
+    rm /etc/my.cnf.d/openstack.cnf
+fi
 cat << EOF >> /etc/my.cnf.d/openstack.cnf
 bind-address = 10.10.2.15
 default-storage-engine = innodb
@@ -9,6 +12,9 @@ max_connections = 4096
 collation-server = utf8_general_ci
 character-set-server = utf8
 EOF
+
 systemctl enable mariadb.service
 systemctl start mariadb.service
 mysql_secure_installation
+
+echo "install database done"
--- ntp-compute.sh
+++ ntp-compute.sh
@@ -6,9 +6,11 @@ if [ -e /etc/chrony.conf.backup ]; then
 else
     cp /etc/chrony.conf /etc/chrony.conf.backup
 fi
+# point to controller
 sed -i -e 's/0.centos.pool.ntp.org/controller/'  /etc/chrony.conf
-sed -i -e 's/server 1.centos.pool.ntp.org iburst/\#server 1.centos.pool.ntp.org iburst' /etc/chrony.conf
-sed -i -e 's/server 2.centos.pool.ntp.org iburst/\#server 2.centos.pool.ntp.org iburst' /etc/chrony.conf
-sed -i -e 's/server 3.centos.pool.ntp.org iburst/\#server 3.centos.pool.ntp.org iburst' /etc/chrony.conf
+# comment others
+sed -i -e 's/server 1.centos.pool.ntp.org iburst/#server 1.centos.pool.ntp.org iburst/' /etc/chrony.conf
+sed -i -e 's/server 2.centos.pool.ntp.org iburst/#server 2.centos.pool.ntp.org iburst/' /etc/chrony.conf
+sed -i -e 's/server 3.centos.pool.ntp.org iburst/#server 3.centos.pool.ntp.org iburst/' /etc/chrony.conf
 systemctl enable chronyd.service
 systemctl start chronyd.service
