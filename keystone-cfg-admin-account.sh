0b13ae94370b095f7d9fdfc7b006409a1ab2fdb2
hebinhao1993 <hebinhao1993@outlook.com>
2019-10-10 16:41:31 +0800

    modify keystone-cfg-admin-account.sh

--- keystone-cfg-admin-account.sh
+++ keystone-cfg-admin-account.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 export OS_USERNAME=admin
-export OS_PASSWORD=ADMIN_PASS
+export OS_PASSWORD=123456
 export OS_PROJECT_NAME=admin
 export OS_USER_DOMAIN_NAME=Default
 export OS_PROJECT_DOMAIN_NAME=Default
