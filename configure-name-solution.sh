13500e31dcdfed95d0a95aa97398af4755bcd345
hebinhao1993 <hebinhao1993@outlook.com>
2019-10-10 18:51:56 +0800

    modify configure-name-solution.sh

--- configure-name-solution.sh
+++ configure-name-solution.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+if [ -e /etc/hosts.backup ]; then
+    cp /etc/hosts.backup /etc/hosts
+else
+    cp /etc/hosts /etc/hosts.backup
+fi
 
 cat << EOF >> /etc/hosts
-10.0.2.15 controller
-10.0.2.16 compute1
+10.0.2.4 controller
+10.0.2.5 compute1
 EOF
