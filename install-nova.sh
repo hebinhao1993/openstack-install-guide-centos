16d32143594715b4f463ff888d273c6bfbdb7790
hebinhao1993 <hebinhao1993@outlook.com>
2019-10-11 20:49:01 +0800

    modify install-nova.sh

--- install-nova.sh
+++ install-nova.sh
@@ -13,7 +13,25 @@ fi
 # modify /etc/nova/nova.conf
 cat nova.conf > /etc/nova/nova.conf
 
-#
+# modify /etc/httpd/conf.d/00-nova-placement-api.conf
+# back up /etc/httpd/conf.d/00-nova-placement-api.conf
+if [ -e /etc/httpd/conf.d/00-nova-placement-api.conf.backup ]; then
+    cp /etc/httpd/conf.d/00-nova-placement-api.conf.backup /etc/httpd/conf.d/00-nova-placement-api.conf
+else
+    cp /etc/httpd/conf.d/00-nova-placement-api.conf /etc/httpd/conf.d/00-nova-placement-api.conf.backup
+fi
+cat << EOF >> /etc/httpd/conf.d/00-nova-placement-api.conf
+<Directory /usr/bin>
+   <IfVersion >= 2.4>
+      Require all granted
+   </IfVersion>
+   <IfVersion < 2.4>
+      Order allow,deny
+      Allow from all
+   </IfVersion>
+</Directory>
+EOF
+
 su -s /bin/sh -c "nova-manage api_db sync" nova
 su -s /bin/sh -c "nova-manage cell_v2 map_cell0" nova
 su -s /bin/sh -c "nova-manage cell_v2 create_cell --name=cell1 --verbose" nova
