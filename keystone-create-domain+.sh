b7f3a3b6c5937fad79af259e523c1cb3912af891
hebinhao1993 <hebinhao1993@outlook.com>
2019-10-11 16:29:50 +0800

    add two echo line to keystone-create-domain+.sh

--- keystone-create-domain+.sh
+++ keystone-create-domain+.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
+echo "start"
 openstack domain create --description "An Example Domain" example
 openstack project create --domain default --description "Service Project" service
 openstack project create --domain default --description "Demo Project" myproject
 openstack user create --domain default --password-prompt myuser
 openstack role create myrole
 openstack role add --project myproject --user myuser myrole
+echo "compelte create project user and role."
