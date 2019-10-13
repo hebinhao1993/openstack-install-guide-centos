a9bb4cb88368d1a701df3c34516844ab4002d68f
hebinhao1993 <hebinhao1993@outlook.com>
2019-10-13 20:14:56 +0800

    fix a bug in caused by install-nova.sh

--- install-nova.sh
+++ install-nova.sh
@@ -21,6 +21,7 @@ else
     cp /etc/httpd/conf.d/00-nova-placement-api.conf /etc/httpd/conf.d/00-nova-placement-api.conf.backup
 fi
 cat << EOF >> /etc/httpd/conf.d/00-nova-placement-api.conf
+
 <Directory /usr/bin>
    <IfVersion >= 2.4>
       Require all granted
