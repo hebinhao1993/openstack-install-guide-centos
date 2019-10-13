1500fafa0c0f0986b8ff6a8fcdbb7923fed6f0d7
hebinhao1993 <hebinhao1993@outlook.com>
2019-10-13 19:18:21 +0800

    modify install-neutron-compute.sh

--- install-neutron-compute.sh
+++ install-neutron-compute.sh
@@ -10,9 +10,21 @@ else
     cp /etc/neutron/neutron.conf /etc/neutron/neutron.conf.backup
 fi
 # edit the /etc/neutron/neutron.conf
-cat neutron.conf > /etc/neutron/neutron.conf
+cat neutron-compute.conf > /etc/neutron/neutron.conf
+ 
+# backup /etc/neutron/plugins/ml2/linuxbridge_agent.ini
+if [ -e /etc/neutron/plugins/ml2/linuxbridge_agent.ini.backup ]; then
+    cp /etc/neutron/plugins/ml2/linuxbridge_agent.ini.backup /etc/neutron/plugins/ml2/linuxbridge_agent.ini
+else
+    cp /etc/neutron/plugins/ml2/linuxbridge_agent.ini /etc/neutron/plugins/ml2/linuxbridge_agent.ini.backup
+fi
+# edit /etc/neutron/plugins/ml2/linuxbridge_agent.ini
+cat linuxbridge_agent-compute.ini > /etc/neutron/plugins/ml2/linuxbridge_agent.ini
 
-# edit the /etc/nova/nova.conf
+# Ensure your Linux operating system kernel supports network bridge filters by verifying all the following sysctl values are set to 1:
+# net.bridge.bridge-nf-call-iptables
+# net.bridge.bridge-nf-call-ip6tables
+# modprobe br_netfilter
 
 # finalize installation
 systemctl restart openstack-nova-compute.service
