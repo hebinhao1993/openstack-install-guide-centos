f95187460ec6a3f6f20dc09df7f76e56508e7eb3
hebinhao1993 <hebinhao1993@outlook.com>
2019-10-13 20:39:53 +0800

    modify install-cinder.sh

--- install-cinder.sh
+++ install-cinder.sh
@@ -4,7 +4,13 @@ set -e
 yum install -y openstack-cinder
 
 # backup /etc/cinder/cinder.conf
+if [ -e /etc/cinder/cinder.conf.backup ]; then
+    cp /etc/cinder/cinder.conf.backup /etc/cinder/cinder.conf
+else
+    cp /etc/cinder/cinder.conf /etc/cinder/cinder.conf.backup
+fi
 # edit /etc/cinder/cinder.conf
+cat cinder.conf > /etc/cinder/cinder.conf
 
 # here we do not edit /etc/nova/nova.conf, since we have edited it when nova is installed
 # ...
